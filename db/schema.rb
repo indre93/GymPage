4fa7c6fff45cdde3ee0c385f3b36d863ff75e4e6
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-08 18:21:26 -0500

    add migration and add seed file to test models associations

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_08_231616) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "workout_id"
+    t.text "content"
+    t.datetime "posted_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["workout_id"], name: "index_comments_on_workout_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "height"
+    t.float "weight"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.text "description"
+    t.boolean "upper_body?"
+    t.boolean "lower_body?"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_workouts_on_user_id"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users:
+
+indre = User.create(first_name: "Indre", last_name: "Bakeer", email: "indre@email.com", height: "5'3", weight: "122", password: "password")
+john = User.create(first_name: "John", last_name: "Smith", email: "john@email.com", height: "5'11", weight: "160", password: "password")
+
+# Workouts:
+
+indre.workouts.create(name: "Planks", description: "3 sets of 2 minutes", upper_body?: true, lower_body?: true)
+
+indre.workouts.create(name: "Squats", description: "5 sets of 10 reps - 180 pounds", upper_body?: true, lower_body?: true)
+
+john.workouts.create(name: "Push-Ups", description: "3 sets / 15 reps", upper_body?: true, lower_body?: true)
