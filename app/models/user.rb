b22f9bd5662416377dd7937fc169f4f9f7b7e888
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-08 21:57:53 -0500

    add validations for User model to verify uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :workouts
   has_many :comments, through: :workouts
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+
+  before_save { self.email = email.downcase }
+
 end
