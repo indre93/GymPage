8993dbc10cdb6c2792064173af08c61dadb46b58
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-09 02:37:17 -0500

    add new and create action for user to login and destroy action for user to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,28 @@ class SessionsController < ApplicationController
 
   def welcome
   end
-  
+
+  def new
+  end
+
+  def create
+    # Find user in our db
+    @user = User.find_by(email: params[:user][:email])
+    # If @user true authenticate password
+    if @user.try(:authenticate, params[:user][:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome back #{@user.first_name}!"
+      redirect_to @user
+    else
+      flash[:error] = "Sorry! The information you have entered in invalid. Please try again."
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    flash[:notice] = "See ya next time!"
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
 end
