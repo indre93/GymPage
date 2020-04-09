3744dbf90f03a9e5ec6838b2c4a3a725bc794c82
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-08 22:03:59 -0500

    add users controller actions and user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :height, :weight, :password)
+  end
+
 end
