11be87603cd22e3f0eb65d109fce52381bfa0292
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-09 02:45:51 -0500

    add create action method to create new user & update user_params method with updated params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,27 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.create(user_params)
+    if @user 
+      session[:user_id] = @user.id
+      flash[:notice] = "Account was successfully created. Welcome to GymPage #{@user.first_name}!"
+      redirect_to @user
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
-
-  end
-
-  def edit
-  end
-
-  def update
+    find_user
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :height, :weight, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
 end
