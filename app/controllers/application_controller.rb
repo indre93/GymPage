f457ff78e3aef0e5de76246a76ef8baaa4cea7a6
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-08 22:54:22 -0500

    add current_user and logged_in? methods and include them as helper_methods to use them in views as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Makes methods available in views as well
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
