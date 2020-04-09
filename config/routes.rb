08cb5fd17f8ecd0cfc546a2982365a00279d7496
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-09 16:18:53 -0500

    simplified sessionscontroller routes and comment out unused ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
-  scope 'user' do
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    delete '/logout' => 'sessions#destroy'
-  end
-  
-  # This will provide you with URLs such as /bob/articles/1 and will allow you to reference the username part of the path as params[:username] in controllers, helpers, and views.
-
-  #scope ':first_name' do
-    #resources :workout
-  #end
-
-  resources :comments
-  resources :workouts
+  #resources :comments
+  #resources :workouts
   resources :users
 end
