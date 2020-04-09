0821c09ace9be1875a02a7dea7dfcbb1a84d0dee
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-08 22:03:06 -0500

    add routes for log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     #resources :workout
   #end
 
-  #resources :comments
-  #resources :workouts
+  resources :comments
+  resources :workouts
   resources :users
 end
