bb5c20fc1e67d38399053e960dfc2b1f4577a746
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 16:04:14 -0500

    add user has_many comments -for comments created by User and add has_many exercises through workouts to keep record of exercises per workout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :workouts
+  has_many :comments
+  has_many :exercises, through: :workouts
   has_many :comments, through: :workouts
 
   validates :first_name, :last_name, :email, presence: true
