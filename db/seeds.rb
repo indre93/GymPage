427755bc720ddd042052e213407566d94154a61e
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 17:01:42 -0500

    change create to create! to prevent silent failures when trying to save new objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,20 @@
 # Users:
-katie = User.create(first_name: "Katie", last_name: "Johnson", email: "katie@email.com", password: "password")
-john = User.create(first_name: "John", last_name: "Smith", email: "john@email.com", password: "password")
+katie = User.create!(first_name: "Katie", last_name: "Johnson", email: "katie@email.com", password: "password")
+john = User.create!(first_name: "John", last_name: "Smith", email: "john@email.com", password: "password")
 
 # Workout: 
-katies_workout = katie.workouts.create(date_workedout: "04/10/2020", description: "Today's workout consisted of a little bit of everything")
-johns_workout = john.workouts.create(date_workedout: "04/09/2020", description: "Simple cardio day. Decided to take a day off from weight lifting.")
+katie.workouts.create!(date_workedout: "04/10/2020", description: "Today's workout consisted of a little bit of everything")
+john.workouts.create!(date_workedout: "04/09/2020", description: "Simple cardio day. Decided to take a day off from weight lifting.")
 
 # Exercises:
-katies_exercises = katies_workout.exercises.create(
+Exercise.create!(
     [
-      { name: "Planks", upper_body?: true, lower_body?: true, cardio?: false },
-      { name: "Bench Press", upper_body?: true, lower_body?: false, cardio?: false },
-      { name: "Pull-up", upper_body?: true, lower_body?: false, cardio?: false },
-      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true },
-      { name: "Squat", upper_body?: true, lower_body?: true, cardio?: false }
-    ]
-  )
-
-johns_exercises = johns_workout.exercises.create(
-    [
-      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true },
-      { name: "Jumping Jacks", upper_body?: true, lower_body?: true, cardio?: true }
+      { name: "Planks", upper_body?: true, lower_body?: true, cardio?: false, user_id: 1, workout_id: 1 },
+      { name: "Bench Press", upper_body?: true, lower_body?: false, cardio?: false, user_id: 1, workout_id: 1 },
+      { name: "Pull-up", upper_body?: true, lower_body?: false, cardio?: false, user_id: 1, workout_id: 1 },
+      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true, user_id: 1, workout_id: 1 },
+      { name: "Squat", upper_body?: true, lower_body?: true, cardio?: false, user_id: 1, workout_id: 1 },
+      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true, user_id: 2, workout_id: 2 },
+      { name: "Jumping Jacks", upper_body?: true, lower_body?: true, cardio?: true, user_id: 2, workout_id: 2 }
     ]
   )
