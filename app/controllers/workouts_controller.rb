456f5383847bd5c51b2edd6ca005d1ce31b52440
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 16:16:35 -0500

    add new and create action for new workouts created by user, and add workout params method)

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,18 @@
 class WorkoutsController < ApplicationController
+
+  def new
+    @workout = Workout.new
+   # @workout.build_exercise
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:date_workedout, :description)
+  end
+
 end
