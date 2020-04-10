d0d69f5a51784afbd0003bb868deec6a243df299
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 16:08:53 -0500

    add new seed data with updated info to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # Users:
-
 katie = User.create(first_name: "Katie", last_name: "Johnson", email: "katie@email.com", password: "password")
 john = User.create(first_name: "John", last_name: "Smith", email: "john@email.com", password: "password")
 
-# Workouts:
+# Workout: 
+katies_workout = katie.workouts.create(date_workedout: "04/10/2020", description: "Today's workout consisted of a little bit of everything")
+johns_workout = john.workouts.create(date_workedout: "04/09/2020", description: "Simple cardio day. Decided to take a day off from weight lifting.")
 
-katie.workouts.create(name: "Planks", description: "3 sets of 2 minutes", upper_body?: true, lower_body?: true)
+# Exercises:
+katies_exercises = katies_workout.exercises.create(
+    [
+      { name: "Planks", upper_body?: true, lower_body?: true, cardio?: false },
+      { name: "Bench Press", upper_body?: true, lower_body?: false, cardio?: false },
+      { name: "Pull-up", upper_body?: true, lower_body?: false, cardio?: false },
+      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true },
+      { name: "Squat", upper_body?: true, lower_body?: true, cardio?: false }
+    ]
+  )
 
-katie.workouts.create(name: "Squats", description: "5 sets of 10 reps - 180 pounds", upper_body?: true, lower_body?: true)
-
-john.workouts.create(name: "Push-Ups", description: "3 sets / 15 reps", upper_body?: true, lower_body?: true)
+johns_exercises = johns_workout.exercises.create(
+    [
+      { name: "Treadmill", upper_body?: true, lower_body?: true, cardio?: true },
+      { name: "Jumping Jacks", upper_body?: true, lower_body?: true, cardio?: true }
+    ]
+  )
