8772bb09696af9a9c2181f656878b4c752c7ab13
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 16:07:22 -0500

    add has_many exercises association to keep track all exercises per workout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,8 @@
 class Workout < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :exercises
   has_many :users, through: :comments
+
+  #accepts_nested_attributes_for :exercise
 end
