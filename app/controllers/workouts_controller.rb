1e1335e47fc8c8111dbe8d7cfbcedb48949695f9
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 20:51:42 -0500

    add show action and find_workout method

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -9,10 +9,20 @@ class WorkoutsController < ApplicationController
     
   end
 
+  def show
+    find_workout
+    redirect_if_not_a_user
+  end
+
+
   private
 
   def workout_params
     params.require(:workout).permit(:date_workedout, :description)
+  end
+
+  def find_workout
+    @workout = Workout.find_by(id: params[:id])
   end
 
 end
