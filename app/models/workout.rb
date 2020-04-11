9f95db5fafe3479744761d3e1d88759ac5922da5
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-10 20:50:50 -0500

    add formatted_date to format date_worked out attribute to make it user friendly

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,4 +3,9 @@ class Workout < ApplicationRecord
   has_many :exercises
   has_many :comments
   has_many :users, through: :comments
+
+  def formatted_date
+    @date = self.date_workedout
+    @date.strftime("%A, %B #{@date.day.ordinalize}")
+  end
 end
