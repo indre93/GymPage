066fcf8ddf6538d33465323611c9a839bee51826
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-12 17:15:40 -0500

    fix associations and add full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :workouts
-  has_many :comments
-  has_many :exercises, through: :workouts
-  has_many :comments, through: :workouts
+  has_many :created_exercises, foreign_key: "user_id", class_name: "Exercise"
+  has_many :created_comments, foreign_key: "user_id", class_name: "Comment"
+  has_many :exercises, through: :workouts # has many completed exercises
+  has_many :comments, through: :workouts # has many commented posts
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
 
   before_save { self.email = email.downcase }
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
