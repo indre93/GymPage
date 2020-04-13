45165c66a675f6b63b9b0c5c0898eed7d39d3a1f
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-13 03:35:23 -0500

    add exercise controllers actions new, create and show

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,6 +2,35 @@ class ExercisesController < ApplicationController
 
   def index
     @exercises = Exercise.all
+    redirect_if_not_a_user
+  end
+
+  def new 
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = current_user.created_exercises.build(exercise_params)
+    if @exercise.save
+      redirect_to exercise_path(@exercise)
+    else
+      render :new
+    end
+  end
+
+  def show
+    find_exercise
+    redirect_if_not_a_user
+  end
+
+  private
+  
+  def exercise_params
+    params.require(:exercise).permit(:name, :category)
+  end
+  
+  def find_exercise
+    @exercise = Exercise.find_by(id: params[:id])
   end
 
 end
