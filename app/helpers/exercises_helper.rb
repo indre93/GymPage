bf9d702e0eef70647a67331ff41d4722a774e4a9
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-13 19:04:35 -0500

    add created at for each exercise in index page, use helper method - in desc order

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -3,5 +3,10 @@ module ExercisesHelper
   def category_options
     ["Upper Body Training", "Lower Body Training", "Cardiovascular Training", "Resistance Training", "Flexibility Training"]
   end
+
+  def formatted_created_at(exercise)
+    date = exercise.created_at
+    date.strftime("%A, %B #{date.day.ordinalize} at %-I:%M %p")
+  end
   
 end
