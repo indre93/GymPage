b5d280d0aebb89041273f77d7b755aac0148f92f
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-13 19:05:37 -0500

    remove formatted_date method and moved it to workout helper

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -5,9 +5,5 @@ class Workout < ApplicationRecord
 
   validates :date, :description, :duration, presence: true
   validates :date, uniqueness: true
-
-  def formatted_date
-    @date = self.date
-    @date.strftime("%A, %B #{@date.day.ordinalize}")
-  end
+  
 end
