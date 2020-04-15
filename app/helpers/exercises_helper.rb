b1088ab55561fcd079c6dbbf37996be7a89b5a6d
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-14 21:12:33 -0500

    rename helper method name to posted_at to avoid confusion

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -4,9 +4,9 @@ module ExercisesHelper
     ["Upper Body Training", "Lower Body Training", "Cardiovascular Training", "Resistance Training", "Flexibility Training"]
   end
 
-  def formatted_created_at(exercise)
+  def formatted_posted_at(exercise)
     date = exercise.created_at
-    date.strftime("%A, %B #{date.day.ordinalize} at %-I:%M %p")
+    date.strftime("%A, %B #{date.day.ordinalize} at %-I:%M%P")
   end
   
 end
