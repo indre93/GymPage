ac1b58e977e7a58c9e9fb5fce71ad047b2ec02f8
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-15 03:14:38 -0500

    add customized exercises_attributes= method to avoid duplicates and make sure attribute values are not blank

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,5 +4,13 @@ class Workout < ApplicationRecord
   has_many :exercises, through: :routines
 
   validates :date, :description, :duration, presence: true
-  
+
+  def exercises_attributes=(attributes)
+    attributes.values.each do |value| 
+      if !value["name"].blank? && !value["category"].blank?
+        self.exercises << Exercise.find_or_create_by(value) 
+      end
+    end
+  end
+
 end
