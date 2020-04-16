f8a5d979a480211d1641ca8ff67081103e56ebd0
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-16 01:02:44 -0500

    fix associations - user has many routines through workouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :workouts
   has_many :created_exercises, foreign_key: "user_id", class_name: "Exercise"
   has_many :exercises, through: :workouts
-  has_many :routines, through: :exercises
+  has_many :routines, through: :workouts
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
