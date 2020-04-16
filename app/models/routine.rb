3747de12ae432954b72a854cd3dd9d9edb2433d5
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-16 01:01:27 -0500

    add exercise_attributes= writer method and validation for the presence of caption

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,13 @@
 class Routine < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
+
+  validates :caption, presence: true
+
+  def exercise_attributes=(attributes)
+    if !attributes.values.any?(&:empty?)
+      self.exercise = Exercise.find_or_create_by(attributes) 
+    end
+  end
+  
 end
