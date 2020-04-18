1814559686cc9547e7040a1041429241af79ed39
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-18 02:22:38 -0500

    add index action to either show all routines or all routines for a particular exercise

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,2 +1,24 @@
 class RoutinesController < ApplicationController
+
+  def index
+    exercise = Exercise.find_by(id: params[:exercise_id])
+    if exercise
+      @routines = exercise.routines
+    else
+      @routines = Routine.all.includes(:workout, :exercise)
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private 
+
+  def routine_params
+    params.require(:routine).permit(:caption)
+  end
+
 end
