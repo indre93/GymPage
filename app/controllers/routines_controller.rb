23054d934b3c12854634ece110225a759850abc5
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-23 23:10:14 -0500

    updated index action -changed exercise to instance variable to make it available in the routines index page- also removed all unused actions and methods

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,24 +1,12 @@
 class RoutinesController < ApplicationController
 
   def index
-    exercise = Exercise.find_by(id: params[:exercise_id])
-    if params[:exercise_id] && exercise
-      @routines = exercise.routines
+    @exercise = Exercise.find_by(id: params[:exercise_id])
+    if params[:exercise_id] && @exercise
+      @routines = @exercise.routines
     else 
-      @routines = Routine.all
+      @routines = Routine.all.includes(:workout, :exercise)
     end
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  private 
-
-  def routine_params
-    params.require(:routine).permit(:caption)
   end
 
 end
