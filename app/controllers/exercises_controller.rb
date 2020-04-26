6af519796686efb930b4a9f62fcb730c74ab3b99
Indre Bakeer <indre.bakeer@gmail.com>
2020-04-26 00:36:41 -0500

    removed new exercise form, and new and create action as well (updated routes to reflect this) - user will be able to add exercise through workout and routine form instead

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,37 +1,12 @@
 class ExercisesController < ApplicationController
-  before_action :require_login
+   before_action :require_login
 
-  def index
-    @exercises = Exercise.all
-  end
+   def index
+      @exercises = Exercise.all
+   end
 
-  def new 
-    @exercise = Exercise.new
-  end
-
-  def create
-    @exercise = Exercise.new(exercise_params)
-    if @exercise.save
-      flash[:message] = "Exercise has been successfully added!"
-      redirect_to exercise_path(@exercise)
-    else
-      flash[:error] = "Unable to add exercise: #{@exercise.errors.full_messages.to_sentence}"
-      render :new
-    end
-  end
-
-  def show
-    find_exercise
-  end
-
-  private
-  
-  def exercise_params
-    params.require(:exercise).permit(:name, :category)
-  end
-  
-  def find_exercise
-    @exercise = Exercise.find_by(id: params[:id])
-  end
+   def show
+      @exercise = Exercise.find_by(id: params[:id])
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-  root 'sessions#welcome'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+   root 'sessions#welcome'
+   get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   delete '/logout' => 'sessions#destroy'
 
-  resources :users, only: [:new, :create, :show] do
-    resources :workouts, only: [:show]
-  end
-  
-  resources :workouts, only: [:new, :create, :show] do
-    resources :routines, only: [:new, :create]
-  end
+   resources :users, only: [:new, :create, :show] do
+      resources :workouts, only: [:show]
+   end
+   
+   resources :workouts, only: [:new, :create, :show] do
+      resources :routines, only: [:new, :create]
+   end
 
-  resources :exercises, only: [:index, :new, :create, :show] do
-    resources :routines, only: [:index]
-  end
-  
-  resources :routines, only: [:index]
+   resources :exercises, only: [:index, :show] do
+      resources :routines, only: [:index]
+   end
+   
+   resources :routines, only: [:index]
+   
 end
