0670e1ea5b229ea144e1ff60c12257c40a0bb98f
Indre Bakeer <indre.bakeer@gmail.com>
2020-05-03 20:15:42 -0500

    add code to avoid errors when using google omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
    provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET'],
       {
-         :prompt => "select_account"
+         :prompt => "select_account",
+         skip_jwt: true
       }
 end
